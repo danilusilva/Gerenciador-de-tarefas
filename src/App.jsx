79d6dc685260765759d32caf023bb241bb34aa17
Danilu Silva <dan7zada@gmail.com>
2025-02-04 17:12:03 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import "tailwindcss";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTasks";
+import { useState } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      description: "Aprender a utilizar o react",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Java",
+      description: "Aprender a utilizar o Java",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar Python",
+      description: "Aprender a utilizar o Python",
+      isCompleted: false,
+    },
+  ]);
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
